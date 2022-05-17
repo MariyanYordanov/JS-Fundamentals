5a3c270d805d2483e061bca432794c151d18fdf9
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-17 23:59:32 +0300

    Add rounding

---
+++ 01.JSBasics_Sintax_Conditional_Statments_And_Loops/Exercise-JS-Basic-Syntax/02.Rounding.js
@@ -0,0 +1,16 @@
+function rounding(N1,N2){
+    let precision = Number(N2) + N1.length;
+    let print = '';
+    if ( precision > 15 ) {
+        precision === 15;
+    }
+    for (let index = 0; index < precision; index++) {
+        if(precision[index]) {
+
+        }
+    }
+    
+    console.log();
+}
+
+rounding(3.1415926535897932384626433832795,2);
