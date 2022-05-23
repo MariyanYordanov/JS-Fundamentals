970526336449f7347ae79c070057fcf676e947dd
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-23 10:16:59 +0300

    Add Towns

---
+++ 02.JS-Data-Type-And-Variables/Exercise-JS-Data-Typeas-And-Variables/03.TownInfo.js
@@ -0,0 +1,8 @@
+function townInfo(town,population,area){
+    console.log(`Town ${town} has population of ${population} and area ${area} square km.`);
+}
+
+townInfo('Sofia',1286383,492); 
+/*
+Town Sofia has population of 1286383 and area 492 square km.
+*/
