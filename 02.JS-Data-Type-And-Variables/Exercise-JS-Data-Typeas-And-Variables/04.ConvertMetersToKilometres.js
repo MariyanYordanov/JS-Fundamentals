d8052f44b082fd3843c33c20fb7f6cca21180744
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-23 10:25:49 +0300

    Add tasks

---
+++ 02.JS-Data-Type-And-Variables/Exercise-JS-Data-Typeas-And-Variables/04.ConvertMetersToKilometres.js
@@ -0,0 +1,6 @@
+function convertor(number){
+    let convert = number / 1000;
+    console.log(convert.toFixed(2));
+}
+
+convertor(798); // 0.80
---
+++ 02.JS-Data-Type-And-Variables/Exercise-JS-Data-Typeas-And-Variables/05.PoundsToDollars.js
@@ -0,0 +1,7 @@
+function convertor(number){
+    let result = number * 1.31;
+    console.log(result.toFixed(3));
+}
+
+convertor(80); // 104.800
+convertor(39); // 51.090
