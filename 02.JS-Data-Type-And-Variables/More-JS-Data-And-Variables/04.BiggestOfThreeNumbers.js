42d08c05d00dc4b6d2fabef96ff88de5d93385cb
Mariyan Yordanov <marsrewq@gmail.com>
2022-05-23 13:20:29 +0300

    Add Biggest Number

---
+++ 02.JS-Data-Type-And-Variables/More-JS-Data-And-Variables/04.BiggestOfThreeNumbers.js
@@ -0,0 +1,6 @@
+function biggestOfThreeNumbers(first,second,third){
+    let bigestNumber = Math.max(Math.max(first,second),third);
+    console.log(bigestNumber);
+}
+
+biggestOfThreeNumbers(-2,7,3); // 7
